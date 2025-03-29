cea9e48735fcb1b82f3ee899be38cadf2ff01dc9
Gustavo Rosa <gustavorosa2900@gmail.com>
2025-03-29 02:46:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import os
-import mysql.connector
-from urllib.parse import urlparse
 from datetime import datetime
+import mysql.connector
+import os
+from urllib.parse import urlparse
 
 def conectar_mysql():
     db_url = os.getenv("DATABASE_URL")
     parsed = urlparse(db_url)
-
     return mysql.connector.connect(
         host=parsed.hostname,
         port=parsed.port or 3306,
@@ -16,21 +15,18 @@ def conectar_mysql():
     )
 
 def registrar_execucao():
+    agora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print("⏱️ Registro:", agora)
     try:
         conn = conectar_mysql()
         cursor = conn.cursor()
-
-        agora = datetime.now()
-        cursor.execute("INSERT INTO logs (timestamp) VALUES (%s)", (agora,))
+        cursor.execute("INSERT INTO Registro (timestamp) VALUES (%s)", (agora,))
         conn.commit()
-
-        print(f"✅ Execução registrada em: {agora}")
-
+        print("✅ Registro salvo!")
         cursor.close()
         conn.close()
-
     except Exception as e:
-        print(f"❌ Erro ao registrar execução: {e}")
+        print(f"❌ Erro ao registrar: {e}")
 
 if __name__ == "__main__":
     registrar_execucao()
